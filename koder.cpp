187c9489dacadf931c9116472b67c94685696249
KristianK1 <kikihd11@gmail.com>
2021-11-24 12:16:43 +0100

    koder one version

--- koder.cpp
+++ koder.cpp
@@ -10,7 +10,7 @@
 
 #define Ncombs 37
 
-char* Popis[Ncombs] = {
+char* Popis[37] = {
 	"and",
 	" to",
 	" an",
@@ -47,27 +47,83 @@ char* Popis[Ncombs] = {
 	"n ",
 	"on",
 	"nd",
-	" o",
+	" o"
 };
 
+char tekst2[3];
+char tekst3[4];
+char tekst4[5];
+char tekst5[6];
+	
 //char tekst[] = "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.";
 
+int find(){
+	for(int i = 0; i<Ncombs; i++){
+		if(strcmp(Popis[i], tekst5)==0){
+			//printf("Naden za 5  |%s|\n", tekst5);
+			return 4;
+		}
+		if(strcmp(Popis[i], tekst4)==0){
+			//printf("Naden za 4  |%s|\n", tekst4);
+			return 3;
+		}
+		if(strcmp(Popis[i], tekst3)==0){
+			//printf("Naden za 3  |%s|\n", tekst3);
+			return 2;
+		}
+		if(strcmp(Popis[i], tekst2)==0){
+			//printf("Naden za 2  |%s|\n", tekst2);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+char next(FILE *f){
+	char c=fgetc(f);
+	if(c==EOF) return EOF;
+
+	tekst2[0]=tekst2[1];
+	tekst2[1]=c;
+	
+	tekst3[0]=tekst3[1];
+	tekst3[1]=tekst3[2];
+	tekst3[2]=c;
+	
+	tekst4[0]=tekst4[1];
+	tekst4[1]=tekst4[2];
+	tekst4[2]=tekst4[3];
+	tekst4[3]=c;
+	
+	tekst5[0]=tekst5[1];
+	tekst5[1]=tekst5[2];
+	tekst5[2]=tekst5[3];
+	tekst5[3]=tekst5[4];
+	tekst5[4]=c;
+	
+//	printf("%s\n", tekst2);
+//	printf("%s\n", tekst3);
+//	printf("%s\n", tekst4);
+//	printf("%s\n\n", tekst5);
+	return c;
+}
+
+
+
 int main(){
-//	for(int i=0;i<37;i++){
-//		printf("|%s|\n", Popis[i]);
-//	}
+	int saved_places = 0;
 	
-	char tekst2[3];
-	char tekst3[4];
-	tekst2[2]='\0';
-	tekst3[3]='\0';
-	
-	
-	FILE *f = fopen("testFile.txt","r");
-//	FILE *f = fopen("Harry Potter books/TXT/HP1.txt","r");
+//	FILE *f = fopen("testFile.txt","r");
+	FILE *f = fopen("Harry Potter books/TXT/HP_ALL.txt","r");
 //	FILE *f = fopen("bible.txt","r");
 //	FILE *f = fopen("TFIOS.txt","r");
 //	FILE *f = fopen("gatsby.txt","r");
+
+	tekst2[2]='\0';
+	tekst3[3]='\0';
+	tekst4[4]='\0';
+	tekst5[5]='\0';
+	
 
 	if(f==NULL){
 		printf("nema fajla");
@@ -77,22 +133,47 @@ int main(){
 	char c = fgetc(f);
 	tekst2[0] = c;
 	tekst3[0] = c;
+	tekst4[0] = c;
+	tekst5[0] = c;
 	c = fgetc(f);
 	tekst2[1] = c;
 	tekst3[1] = c;
+	tekst4[1] = c;
+	tekst5[1] = c;
 	c = fgetc(f);
 	tekst3[2] = c;
+	tekst4[2] = c;
+	tekst5[2] = c;
+	c = fgetc(f);
+	tekst4[3] = c;
+	tekst5[3] = c;
+	c = fgetc(f);
+	tekst5[4] = c;
 	
-	printf("%s\n\n", tekst2);
-	for(int i=0;i<Ncombs; i++){
-		if(strcmp(tekst2, Popis[i])==0){
-			printf("lol %d |%s| \n",i, Popis[i]);
+	int saved = find();
+	saved_places+=saved;
+	for(int i=0;i<saved;i++) next(f);
+	
+	
+	int n=0;
+	while(1){
+		//printf("NEW LOOP\n\n");
+		
+		if(next(f)==EOF) {
+			break;
 		}
+		n++;
+		
+		saved = find();
+		saved_places+=saved;
+		for(int i=0;i<saved;i++) {
+			n++;
+			if(next(f)==EOF) break;
+		}
+		
 	}
-	
-	
-	
-	
+	printf("saved: %d, total: %d. %.2f\n", saved_places, n, 100.0*saved_places/n);
+	return 0;
 }
 
 
